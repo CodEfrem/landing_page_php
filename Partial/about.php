51ed437053beea0fa392d16a12e955affd6d4fd8
Efrem YOUSSOUF <youssouf.efrem@gmail.com>
2023-12-07 15:21:27 +0100

    LandingPagePhP

---
+++ Partial/about.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+echo '
+<section class="page-section bg-primary text-white mb-0" id="about">
+    <div class="container">
+        
+        <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+        
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-4 ms-auto"><p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit suscipit tellus. Proin auctor hendrerit blandit. Quisque luctus, ligula nec consectetur scelerisque, nisl justo efficitur orci, eu luctus libero felis id mauris.</p></div>
+            <div class="col-lg-4 me-auto"><p class="lead">Morbi blandit dapibus diam, sed efficitur quam cursus at. Nulla ac arcu vel felis cursus elementum. In vel sagittis nisi. Integer laoreet in sem eu gravida. Mauris laoreet quam non leo volutpat, eget lacinia neque scelerisque.</p></div>
+        </div>
+    </div>
+</section>'
+
+?>
---
+++ Partial/copyright.php
@@ -0,0 +1,8 @@
+<?php
+
+echo'
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © <?php echo date("Y"); ?> Your Name. All rights reserved.
+</div>';
+
+?>
